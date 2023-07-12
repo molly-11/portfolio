6e3d22ef7548f69b44fcbf8ec203443dae7bd98e
Edit Selmeczi <selmeczi.e@gmail.com>
2023-07-12 17:34:18 +0200

    add design to lading page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,24 +1,25 @@
-import '../assets/styles/LandingPage.css'
+import "../assets/styles/LandingPage.css";
 
 function CreateLandingPage() {
   return (
     <>
       <nav className="navbar">
-        <div>
-        <div>Logo</div>
-        <div>
-        <ul id='navList'>
-        
-        <li>Projects</li>
-        <li>Contact</li>
-        </ul>
-
+        <div className="logoDiv">
+          <div>Logo</div>
+          <div>
+            <ul id="navList">
+              <li>Projects</li>
+              <li>Contact</li>
+            </ul>
+          </div>
         </div>
-        </div>
-        
-        
       </nav>
-     
+      <div id="landingDiv">
+        <img src="src\assets\images\profile.jpg" alt="Prolife image" />
+        <div>
+          <h3>Hello! I'm Edit.</h3>
+          <p id="introP">I'm a front-end developer based in Budapest. I love combining the worlds of logic and creative design to make engaging and entertaining user experiences.</p></div>
+      </div>
     </>
   );
 }
