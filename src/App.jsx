85c6cd810c7ed90f41c9ae4119488c8b401b74ff
Edit Selmeczi <selmeczi.e@gmail.com>
2023-07-12 16:13:39 +0200

    create initial document with nav bar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './assets/styles/App.css'
+import './components/LandingPage'
+import CreateLandingPage from './components/LandingPage'
+
+function App() {
+ 
+  return (
+    <>
+      <CreateLandingPage/>
+      <h1>Portfolio</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,26 @@
+import '../assets/styles/LandingPage.css'
+
+function CreateLandingPage() {
+  return (
+    <>
+      <nav className="navbar">
+        <div>
+        <div>Logo</div>
+        <div>
+        <ul id='navList'>
+        
+        <li>Projects</li>
+        <li>Contact</li>
+        </ul>
+
+        </div>
+        </div>
+        
+        
+      </nav>
+     
+    </>
+  );
+}
+
+export default CreateLandingPage;
